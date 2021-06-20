81bb47a4a761748b80a93cfd8f04a341e25cdf4c
butch <au.bouchard@gmail.com>
2021-06-20 18:24:03 +0200

    article to article js

--- js/article.js
+++ js/article.js
@@ -1,8 +1,7 @@
-const test = document.getElementById("test");
+const mainCol = document.getElementById("main");
 
 // get the id of the current article from the url
 // i.e. http://192.168.0.200:5500/pages/article.html#5be9cc611c9d440000c1421e
-
 
 function getTheIdFromURL(URL) {
 	return URL.slice(URL.indexOf("#")+1);
@@ -15,29 +14,39 @@ const id = getTheIdFromURL(document.URL);
 
 const fetchMyAPI = fetch(`http://localhost:3000/api/furniture/${id}`);
 
+
+// affichage de l'article dans la partie centrale
 fetchMyAPI
 	.then( returnedResponse => {
-		// deal with “returnedResponse”
 		returnedResponse.json()
 			.then( articleObject => {
-				console.log(articleObject)
 
+					main.innerHTML = `				
 
-					test.appendChild( document.createElement("div"));
-					test.lastElementChild.setAttribute("class", "col mt-5");
-					test.lastElementChild.innerHTML = `						
-							<div class="card article">
-							
-								<div class="card-header ">
-									<h5 class="card-title d-flex justify-content-between">${articleObject.name}<span class="price">${articleObject.price/100} €</span></h5>
-								</div>
-
-								<img src="${articleObject.imageUrl}" class="card-img-top">
-								
-								<div class="card-body">
-									<p class="card-text">${articleObject.description}</p>
-								</div>
+						<div class="row">
+							<div class="col-md-8 m-3 shadow p-0">
+								<a href="${articleObject.imageUrl}" rel="noreferrer noopener" target="_blank">
+								<img src="${articleObject.imageUrl}" alt="meuble ${articleObject.name}" class="img-fluid">
+								</a>
 							</div>
+
+							<div class="col m-3">
+								<p class="h2">${articleObject.name}</p>
+								<p class="h3">${articleObject.price/100} €</p>
+								</br>
+								<p class="h5">Personnalisation :</p>
+								<button id="addToBasketBtn" class="btn p-3">Ajouter au panier</button>
+							</div>
+						</div>
+
+						<div class="row">
+							<div class="col m-3">
+								<p class="h4">Description :</p>
+								<p class="h5">${articleObject.description}</p>
+							</div>
+						</div>
+
+
 					`;
 			});
 	} )
@@ -45,3 +54,43 @@ fetchMyAPI
 		// deal with “returnedError”
 		console.log(returnedError.json());
 	} );
+
+
+
+// récupération des thumnails pour l'aside
+const asideCol = document.getElementById("aside");
+
+const fetchApiForImages = fetch(`http://localhost:3000/api/furniture`);
+
+fetchApiForImages
+	.then( returnedResponse => {
+		// deal with “returnedResponse”
+		returnedResponse.json()
+			.then( arrayOfObjects => {
+				arrayOfObjects.forEach(element => {
+					// deal with each element as object article
+
+					asideCol.appendChild( document.createElement("div"));
+					asideCol.lastElementChild.setAttribute("class", "row my-3 pr-sm-2 pr-md-3 pr-lg-4");
+					asideCol.lastElementChild.innerHTML = `
+						<a href="article.html#${element._id}" class="shadow">
+						<img class="img-thumbnail" src="${element.imageUrl}">
+						</a>
+					`;
+
+					// prise en compte du besoin de rafraichissement pour chaque lien
+
+					asideCol.lastElementChild.addEventListener('click', refresh);
+				});
+			});
+	} )
+	.catch( returnedError => {
+		// deal with “returnedError”
+		console.log(returnedError.json());
+	} );
+
+
+function refresh() {
+	console.log("refresh");
+	setTimeout(function() {document.location.reload()},30);
+}
