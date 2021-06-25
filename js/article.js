d37be617db98403834142724aad3f28660976178
butch <au.bouchard@gmail.com>
2021-06-25 09:57:32 +0200

    separate functions in home and article

--- js/article.js
+++ js/article.js
@@ -1,55 +1,15 @@
 // demande d'un article à l'API : GET http://localhost:3000/api/:id
-const id = getTheIdFromURL(document.URL);
+const productId = getTheIdFromURL();
 
-const fetchMyAPI = fetch(`http://localhost:3000/api/furniture/${id}`);
+const fetchMyAPI = fetch(`http://localhost:3000/api/furniture/${productId}`);
 
 
 // affichage de l'article dans la partie centrale
 fetchMyAPI
-	.then( returnedResponse => {
-		returnedResponse.json()
-			.then( articleObject => {
-
-				let linkToPicture = document.getElementById("linkToPicture");
-				linkToPicture.setAttribute("href", `${articleObject.imageUrl}`);
-
-
-				let mainPicture = document.getElementById("mainPicture");
-				mainPicture.setAttribute("src",`${articleObject.imageUrl}`);
-				mainPicture.setAttribute("alt",`meuble ${articleObject.name}`);
-
-
-				let pTitle = document.getElementById("pTitle");
-				pTitle.textContent = `${articleObject.name}`;
-
-
-				let pPrice = document.getElementById("pPrice");
-				pPrice.setAttribute("value", `${articleObject.price}`);
-				pPrice.textContent = `${articleObject.price/100} €`;
-
-
-				let firstOption = document.createElement("option");
-				firstOption.setAttribute("selected", "true");
-				firstOption.textContent = `${articleObject.varnish[0]}`;
-
-				let optionSelector = document.getElementById("optionSelector");
-				optionSelector.appendChild(firstOption);
-
-
-				// // if more than 1 option :
-
-				for (let i =1; i < articleObject.varnish.length; i++ ) {
-					let nextOption = document.createElement("option");
-					nextOption.textContent = `${articleObject.varnish[i]}`;
-
-					optionSelector.appendChild(nextOption);				
-				}
-
-				let btnAddToBasket = document.getElementById("addToBasketBtn");
-				btnAddToBasket.addEventListener("click", addToBasket);
-
-				let pDescription = document.getElementById("pDescription");
-				pDescription.textContent = `${articleObject.description}`;
+	.then( response => {
+		response.json()
+			.then( furniture => {
+				updateArticle(furniture);
 			});
 	} )
 	.catch( returnedError => {
@@ -65,49 +25,22 @@ const asideCol = document.getElementById("aside");
 const fetchApiForImages = fetch(`http://localhost:3000/api/furniture`);
 
 fetchApiForImages
-	.then( returnedResponse => {
-		// deal with “returnedResponse”
-		returnedResponse.json()
-			.then( arrayOfObjects => {
-				arrayOfObjects.forEach(element => {
-					// deal with each element as object article
-
-					let newDiv = document.createElement("div");
-					newDiv.setAttribute("class", "row my-3 pr-sm-2 pr-md-3 pr-lg-4");
-
-					let linkToPicture = document.createElement("a");
-					linkToPicture.setAttribute("href", `article.html#${element._id}`);
-					linkToPicture.setAttribute("class", "shadow");
-					linkToPicture.setAttribute("title", `${element.name}`);
-
-					let mainPicture = document.createElement("img");
-					mainPicture.setAttribute("class", "img-thumbnail");
-					mainPicture.setAttribute("src", `${element.imageUrl}`);
-					mainPicture.setAttribute("alt", `lien vers ${element.name}`);
-
-					linkToPicture.appendChild(mainPicture);
-					newDiv.appendChild(linkToPicture);					
-					asideCol.appendChild(newDiv);
-
-					asideCol.lastElementChild.addEventListener('click', refresh);
+	.then( response => {
+		response.json()
+			.then( listOfArticles => {
+				listOfArticles.forEach(furniture => {
+					asideCol.appendChild(asideLink(furniture));
 				});
 			});
 	} )
-	.catch( returnedError => {
-		// deal with “returnedError”
-		console.log(returnedError.json());
+	.catch( error => {
+		console.log(error);
 	} );
 
-function getTheIdFromURL(URL) {
-	// get the id of the current article from the url
-	// i.e. http://192.168.0.200:5500/pages/article.html#5be9cc611c9d440000c1421e
-	return URL.slice(URL.indexOf("#")+1);
+function getTheIdFromURL() {
+	return (new URLSearchParams(document.location.search.substring(1))).get("id");
 }
 
-function refresh() {
-	console.log("refresh");
-	setTimeout(function() {document.location.reload()},30);
-}
 
 
 // ADD TO BASKET FUNCTIONS
@@ -115,32 +48,82 @@ function refresh() {
 function addToBasket() {
 	// create an object "article" width id, name, varnish, price and imageUrl :
 
-	// id already exist
-
-	// get name
 	let name = document.getElementById("pTitle").textContent;
-
-	// check wich option is checked
 	let varnish = document.getElementById("optionSelector").value;
-
-	// get price
 	let price = document.getElementById("pPrice").getAttribute("value")*1;
-
-	// get imageUrl
 	let imageUrl = document.getElementById("mainPicture").src;
 
-	const articleInBasket = new ArticleInBasket (id, name, varnish, price, imageUrl);
+	const articleInBasket = new ArticleInBasket (productId, name, varnish, price, imageUrl);
 
 
-	// check localStorage length and a name
+	// check localStorage length and a name ----------------------------------------------------- A CHANGER
 	let itemsInBasket = "article"+localStorage.length;
 
+		// IL FAUT VERIFIER LA PRESENCE D'UN OBJET BASKET
+		// SI ABSENT CREER, PUIS
+		// VERIFIER LA PRESENCE D'UN OBJET IDENTIQUE
+		// AJOUTER L'OBJET : SOIT EN AUGMENTANT LA Q SOIT EN EN CREANT UN NOUVEL OBJET
 
 	// store this in localStorage
 	localStorage.setItem(itemsInBasket, JSON.stringify(articleInBasket));
 
-	// check item properly added then message/alert
-	//		message is : continuer achats ou voir le panier (2 boutons = 2 liens)
-	//		alert is : une erreur s'est produite
 
+}
+
+
+const asideLink = (miniCard) => {
+	let newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "row my-3 pr-sm-2 pr-md-3 pr-lg-4");
+
+	let linkToPicture = document.createElement("a");
+	linkToPicture.setAttribute("href", `article.html?id=${miniCard._id}`);
+	linkToPicture.setAttribute("class", "shadow");
+	linkToPicture.setAttribute("title", `${miniCard.name}`);
+
+	let mainPicture = document.createElement("img");
+	mainPicture.setAttribute("class", "img-thumbnail");
+	mainPicture.setAttribute("src", `${miniCard.imageUrl}`);
+	mainPicture.setAttribute("alt", `lien vers ${miniCard.name}`);
+
+	linkToPicture.appendChild(mainPicture);
+	newDiv.appendChild(linkToPicture);
+
+	return newDiv;
+};
+
+function updateArticle(article) {
+	let linkToPicture = document.getElementById("linkToPicture");
+	linkToPicture.setAttribute("href", `${article.imageUrl}`);
+
+	let mainPicture = document.getElementById("mainPicture");
+	mainPicture.setAttribute("src",`${article.imageUrl}`);
+	mainPicture.setAttribute("alt",`meuble ${article.name}`);
+
+	let pTitle = document.getElementById("pTitle");
+	pTitle.textContent = `${article.name}`;
+
+	let pPrice = document.getElementById("pPrice");
+	pPrice.setAttribute("value", `${article.price}`);
+	pPrice.textContent = `${article.price/100} €`;
+
+	let firstOption = document.createElement("option");
+	firstOption.setAttribute("selected", "true");
+	firstOption.textContent = `${article.varnish[0]}`;
+
+	let optionSelector = document.getElementById("optionSelector");
+	optionSelector.appendChild(firstOption);
+
+	// // if more than 1 option :
+	for (let i =1; i < article.varnish.length; i++ ) {
+		let nextOption = document.createElement("option");
+		nextOption.textContent = `${article.varnish[i]}`;
+
+		optionSelector.appendChild(nextOption);
+	}
+
+	let btnAddToBasket = document.getElementById("addToBasketBtn");
+	btnAddToBasket.addEventListener("click", addToBasket);
+
+	let pDescription = document.getElementById("pDescription");
+	pDescription.textContent = `${article.description}`;
 }
--- js/home.js
+++ js/home.js
@@ -1,74 +1,63 @@
+// import {createCard} from "./templates";
+
 // AFFICHAGE DES ARTICLES MEUBLEs
 const panel = document.getElementById("panel");
 
 
 // demande de la liste des article à l'API : GET http://localhost:3000/api/furniture
 
-const fetchMyAPI = fetch(`http://localhost:3000/api/furniture`);
+const getFurnitures = fetch(`http://localhost:3000/api/furniture`);
 
-fetchMyAPI
-	.then( returnedResponse => {
-		// deal with “returnedResponse”
-		returnedResponse.json()
+getFurnitures
+	.then( response => {
+		response.json()
 			.then( arrayOfObjects => {
-				arrayOfObjects.forEach(element => {
-					// deal with each element as object article
-
-					// panel.appendChild( document.createElement("div"));
-					// panel.lastElementChild.setAttribute("class", "col-fluid col-md-5 m-auto");
-					// panel.lastElementChild.innerHTML = `<a href="article.html#${element._id}" title="${element.name} : ${element.varnish.length} variations" class="text-decoration-none">
-					//
-					// 		<div class="card furniture mb-5">
-					// 			<img src="${element.imageUrl}" class="card-img-top" alt="${element.name}">
-					// 			<div class="card-footer d-flex">
-					// 				<span class="h5 card-title d-inline-block">${element.name}</span>
-					// 				<span class="h5 d-inline-block ml-auto price">${element.price/100} €</span>
-					// 			</div>
-					// 		</div>
-					// </a>
-					// `;
-
-					let newDiv = document.createElement("div");
-					newDiv.setAttribute("class", "col-fluid col-md-5 m-auto");
-
-					let cardLink = document.createElement("a");
-					cardLink.setAttribute("href", `article.html#${element._id}`);
-					cardLink.setAttribute("title", `${element.name} : ${element.varnish.length} variations`);
-					cardLink.setAttribute("class", "text-decoration-none");
-
-					let cardDiv =  document.createElement("div");
-					cardDiv.setAttribute("class", "card furniture mb-5");
-
-					let cardImg = document.createElement("img");
-					cardImg.setAttribute("src",`${element.imageUrl}`)
-					cardImg.setAttribute("class","card-img-top")
-					cardImg.setAttribute("alt",`${element.name}`)
-
-					let cardFooter = document.createElement("div");
-					cardFooter.setAttribute("class","card-footer d-flex");
-
-					let span1 = document.createElement("span");
-					span1.setAttribute("class","h5 card-title d-inline-block");
-					span1.textContent = `${element.name}`;
-
-					let span2 = document.createElement("span");
-					span2.setAttribute("class","h5 d-inline-block ml-auto price");
-					span2.textContent = `${element.price/100} €`;
-
-					newDiv.appendChild(cardLink);
-						cardLink.appendChild(cardDiv);
-							cardDiv.appendChild(cardImg);
-							cardDiv.appendChild(cardFooter);
-								cardFooter.appendChild(span1);
-								cardFooter.appendChild(span2);
-
-					panel.appendChild(newDiv);
-
-
+				arrayOfObjects.forEach(furniture => {
+					panel.appendChild(createCard(furniture));
 				});
 			});
 	} )
-	.catch( returnedError => {
-		// deal with “returnedError”
-		console.log(returnedError.json());
+	.catch( error => {
+		console.log(error);
 	} );
+
+
+export {};
+
+const createCard = (furniture) => {
+	let newDiv = document.createElement("div");
+	newDiv.setAttribute("class", "col-fluid col-md-5 m-auto");
+
+	let cardLink = document.createElement("a");
+	cardLink.setAttribute("href", `article.html?id=${furniture._id}`);
+	cardLink.setAttribute("title", `${furniture.name} : ${furniture.varnish.length} variations`);
+	cardLink.setAttribute("class", "text-decoration-none");
+
+	let cardDiv =  document.createElement("div");
+	cardDiv.setAttribute("class", "card furniture mb-5");
+
+	let cardImg = document.createElement("img");
+	cardImg.setAttribute("src",`${furniture.imageUrl}`)
+	cardImg.setAttribute("class","card-img-top")
+	cardImg.setAttribute("alt",`${furniture.name}`)
+
+	let cardFooter = document.createElement("div");
+	cardFooter.setAttribute("class","card-footer d-flex");
+
+	let span1 = document.createElement("span");
+	span1.setAttribute("class","h5 card-title d-inline-block");
+	span1.textContent = `${furniture.name}`;
+
+	let span2 = document.createElement("span");
+	span2.setAttribute("class","h5 d-inline-block ml-auto price");
+	span2.textContent = `${furniture.price/100} €`;
+
+	newDiv.appendChild(cardLink);
+	cardLink.appendChild(cardDiv);
+	cardDiv.appendChild(cardImg);
+	cardDiv.appendChild(cardFooter);
+	cardFooter.appendChild(span1);
+	cardFooter.appendChild(span2);
+
+	return newDiv;
+}
